995a8a9e573d4c1b2f4d61723a55ba4577d92897
Gowthami2601 <66249633+Gowthami2601@users.noreply.github.com>
2021-10-08 00:02:40 +0530

    Add files via upload
    Single Responsibility Principle

---
+++ Bank.Messages.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BankCon
+{
+    class Bank
+    {
+        public string GetStringInput()
+        {
+            return Console.ReadLine();
+        }
+        public int GetIntInput()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        public int StringInt(string s)
+        {
+            return Convert.ToInt32(s);
+        }
+    }
+}
